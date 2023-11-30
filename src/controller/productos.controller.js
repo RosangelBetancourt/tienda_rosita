708292625d0c547635f3182130362ed9a186c6f7
RosangelBetancourt <betancourtmatuzalenrove@uvm.edu.ve>
2023-11-30 19:30:33 -0400

    Agregamos los endpoints de Productos

--- src/controller/productos.controller.js
+++ src/controller/productos.controller.js
@@ -1,7 +1,299 @@
-const ProductosModel = require('../models/sedes');
+const ProductosModel = require('../models/productos');
+const AlmacenesModel = require('../models/almacenes');
+const CategoriasModel = require('../models/categorias');
+const { elminarDeUnArray } = require('./service/acciones');
+const mongoose = require('mongoose');
+const ProveedoresModel = require('../models/proveedores');
 
 class Producto {
 
+    listar() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const totalProductos = await ProductosModel.find().select(
+                    '_id nombre precio categoria alamcen proveedor cantidad'
+                )
+                return resolve({
+                    ok: true,
+                    totalProductos
+                })
+            } catch (error) {
+                console.error('Error al listar los Productos:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al listar los Productos',
+                })
+            }
+        })
+    }
+
+    listarCategoria(categoria) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const totalProductos = await ProductosModel.find({"almacen": categoria}).select(
+                    '_id nombre precio categoria alamcen proveedor cantidad'
+                )
+                return resolve({
+                    ok: true,
+                    categoria,
+                    "Empleados": totalProductos
+                })
+            } catch (error) {
+                console.error('Error al listar los Productos:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al listar los Productos',
+                })
+            }
+        })
+    }
+
+    listarAlmacen(almacen) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const totalProductos = await ProductosModel.find({"almacen": almacen}).select(
+                    '_id nombre precio categoria alamcen proveedor cantidad'
+                )
+                return resolve({
+                    ok: true,
+                    almacen,
+                    "Empleados": totalProductos
+                })
+            } catch (error) {
+                console.error('Error al listar los Productos:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al listar los Productos',
+                })
+            }
+        })
+    }
+
+    agregar(producto) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (!producto.nombre || !producto.precio || !producto.categoria || !producto.almacen || !producto.proveedor || !producto.cantidad) {
+                    return reject('Faltan propiedades escenciales: nombre, precio, categoria, almacen, proveedor y cantidad')
+                } else {
+
+                    //validamos que el nombre no se repite
+                    const existeProducto = await ProductosModel.findOne({
+                        nombre: producto.nombre
+                    })
+    
+                    if (existeProducto) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'Ya existe un Producto con ese nombre',
+                        })
+                    }
+
+
+                    //validamos que exista la Categoria
+                    const existeCategoria = await CategoriasModel.findOne({
+                        nombre: producto.categoria
+                    })
+
+                    if (!existeCategoria) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'No existe la Categoria que has ingresado, elige una Categoria correcta',
+                        })
+                    }
+
+                    //validamos que exista el Almacen
+                    const existeAlmacen = await AlmacenesModel.findOne({
+                        nombre: producto.almacen
+                    })
+
+                    if (!existeAlmacen) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'No existe la Almacen que has ingresado, elige una Almacen correcto',
+                        })
+                    }
+
+                    //validamos que exista el Proveedor
+                    const existeProveedor = await ProveedoresModel.findOne({
+                        nombre: producto.proveedor
+                    })
+
+                    if (!existeProveedor) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'No existe el Proveedor que has ingresado, elige una Proveedor correcto',
+                        })
+                    }
+
+                    const nuevoProducto = {
+                        nombre: producto.nombre,
+                        precio: producto.precio,
+                        categoria: producto.categoria,
+                        almacen: producto.almacen,
+                        proveedor: producto.proveedor,
+                        cantidad: producto.cantidad
+                    }
+
+                    const productoCreado = await ProductosModel.create(
+                        nuevoProducto
+                    )
+
+                    if (!productoCreado) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'Hubo un error al crear el Producto',
+                        })
+                    }
+
+                    existeCategoria.productos.push(producto.nombre)
+                    await CategoriasModel.updateOne({ nombre: producto.categoria }, { $set: {productos: existeCategoria.productos} })
+
+                    existeAlmacen.productos.push(producto.nombre)
+                    await AlmacenesModel.updateOne({ nombre: producto.almacen }, { $set: {productos: existeAlmacen.productos} })
+
+                    resolve({
+                        ok: true,
+                        "Producto": productoCreado,
+                    })
+                }
+            } catch (error) {
+                console.error('Error al agregar el Producto:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al agregar el Producto',
+                })
+            }
+        })
+    }
+
+    editar(producto, id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //validacion del ID para que sea como el ID de Mongoose
+                if (!mongoose.isValidObjectId(id)) {
+                    return reject({
+                        ok: false,
+                        mensaje: 'ID no válido',
+                    })
+                }
+
+                if (!producto.precio || !producto.almacen) {
+                    return reject('Faltan propiedades escenciales: precio y almacen')
+                } else {
+                    const anteriorProducto = await ProductosModel.findOne({ _id: id });
+                    const almacenAnterior = await AlmacenesModel.findOne({
+                        nombre: anteriorProducto.almacen
+                    })
+
+                    //validamos que exista el Almacen
+                    const existeAlmacen = await AlmacenesModel.findOne({
+                        nombre: producto.almacen
+                    })
+
+                    if (!existeAlmacen) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'No existe el Almacen que has ingresado, elige un Almacen correcto',
+                        })
+                    }
+
+                    const edicionProducto = {
+                        precio: producto.precio,
+                        almacen: producto.almacen
+                    }
+
+                    const productoEditado = await ProductosModel.updateOne({ _id: id }, { $set: edicionProducto })
+
+                    if (!productoEditado) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'Hubo un error al editar el Producto',
+                        })
+                    }
+
+                    const respuesta = elminarDeUnArray(almacenAnterior.productos, anteriorProducto.nombre);
+                    console.log(respuesta)
+                    await AlmacenesModel.updateOne({ nombre: anteriorProducto.almacen }, { productos: respuesta })
+
+                    const almacenNuevo = await AlmacenesModel.findOne({nombre: producto.almacen})
+                    almacenNuevo.productos.push(anteriorProducto.nombre)
+                    await AlmacenesModel.updateOne({ nombre: producto.almacen }, { $set: {productos: almacenNuevo.productos} })
+
+                    resolve({
+                        mensaje: 'Producto Editado',
+                        ok: true,
+                        "Producto": productoEditado,
+                    })
+                }
+            } catch (error) {
+                console.error('Error al editar el Producto:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al editar el Producto',
+                })
+            }
+        })
+    }
+
+
+    eliminar(id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                //validacion del ID para que sea como el ID de Mongoose
+                if (!mongoose.isValidObjectId(id)) {
+                    return reject({
+                        ok: false,
+                        mensaje: 'ID no válido',
+                    })
+                }
+
+                const producto = await ProductosModel.findOne({ _id: id })
+
+                // Eliminamos el Producto seleccionado
+                const productoEliminado = await ProductosModel.findByIdAndDelete(id)
+                if (!productoEliminado) {
+                    return reject({
+                        ok: false,
+                        mensaje: 'Hubo un error al eliminar el Producto',
+                    })
+                }
+
+                //Eliminamos el Producto de la Categoria
+                const Categoria = await CategoriasModel.findOne({
+                    nombre: producto.categoria
+                })
+
+                const respuesta = elminarDeUnArray(Categoria.productos, producto.nombre)
+
+                if (respuesta != true) {
+                    await CategoriasModel.updateOne({ nombre: producto.categoria }, { productos: respuesta })
+                }
+
+                //Eliminamos el Producto del Almacen
+                const Almacen = await AlmacenesModel.findOne({
+                    nombre: producto.almacen
+                })
+
+                const respuesta2 = elminarDeUnArray(Almacen.productos, producto.nombre)
+
+                if (respuesta2 != true) {
+                    await AlmacenesModel.updateOne({ nombre: producto.almacen }, { productos: respuesta2 })
+                }
+
+                return resolve({
+                    ok: true,
+                    productoEliminado,
+                    mensaje: 'Producto eliminado',
+                })
+            } catch (error) {
+                console.error('Error al eliminar el Producto:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al eliminar el Producto',
+                })
+            }
+        })
+    }
 }
 
 const productosC = new Producto();
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,34 +1,110 @@
 var express = require('express');
 var router = express.Router();
+const Productos = require('../controller/productos.controller')
+const checkAutenticacion = require('../controller/service/jwtAuth');
 
 // Listar Productos
-router.get('/listar', function(req, res, next) {
-  
+router.get('/listar', function(req, res, next){
+    let roles = ["admin", "editor", "user"];
+    checkAutenticacion(req, res, next, roles);
+},
+
+function(req, res, next) {
+    Productos.listar()
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 // Listar Productos por Categoria
-router.get('/listar/categorias', function(req, res, next) {
-  
+router.get('/listar/categoria/:categoria', function(req, res, next){
+    let roles = ["admin", "editor", "user"];
+    checkAutenticacion(req, res, next, roles);
+},
+
+function(req, res, next) {
+    const { categoria } = req.params
+
+    Productos.listarCategoria(categoria)
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 // Listar Productos por Almacen
-router.get('/listar/almacenes', function(req, res, next) {
-  
+router.get('/listar/almacen/:almacen', function(req, res, next){
+    let roles = ["admin", "editor", "user"];
+    checkAutenticacion(req, res, next, roles);
+},
+
+function(req, res, next) {
+    const { almacen } = req.params
+
+    Productos.listarAlmacen(almacen)
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 /* Crear Productos. */
-router.post('/agregar', function(req, res, next) {
-  
+router.post('/agregar', function(req, res, next){
+    let roles = ["admin", "editor"];
+    checkAutenticacion(req, res, next, roles);
+},
+
+function(req, res, next) {
+    Productos.agregar(req.body)
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 // Editar Producto
-router.put('/editar', function(req, res, next) {
-  
+router.put('/editar/:id', function(req, res, next){
+    let roles = ["admin", "editor"];
+    checkAutenticacion(req, res, next, roles);
+},
+
+function(req, res, next) {
+    const { id } = req.params
+
+    Productos.editar(req.body, id)
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 // Eliminar Producto
-router.delete('/eliminar', function(req, res, next) {
-  
+router.delete('/eliminar/:id', function(req, res, next){
+    let roles = ["admin"];
+    checkAutenticacion(req, res, next, roles);
+},
+
+function(req, res, next) {
+    const { id } = req.params
+
+    Productos.eliminar(id)
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 module.exports = router;
