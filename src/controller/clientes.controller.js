e7cd0212acc76b6b840021e6453fbf3d31922944
RosangelBetancourt <betancourtmatuzalenrove@uvm.edu.ve>
2023-11-30 20:18:04 -0400

    Agregamos los endpoints de Clientes

--- src/controller/clientes.controller.js
+++ src/controller/clientes.controller.js
@@ -1,7 +1,92 @@
 const ClientesModel = require('../models/clientes');
+const ProductosModel = require('../models/productos');
+const { elminarDeUnArray } = require('./service/acciones');
+const mongoose = require('mongoose');
 
 class Cliente {
-    
+
+    listar() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const totalClientes = await ClientesModel.find().select(
+                    '_id nombre apellido cedula producto gasto'
+                )
+                return resolve({
+                    ok: true,
+                    totalClientes
+                })
+            } catch (error) {
+                console.error('Error al listar los Clientes:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al listar los Clientes',
+                })
+            }
+        })
+    }
+
+    agregar(cliente) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                if (!cliente.nombre || !cliente.apellido || !cliente.cedula || !cliente.producto) {
+                    return reject('Faltan propiedades escenciales: nombre, apellido, cedula y producto')
+                } else {
+                    //validamos que exista la sede
+                    const existeProducto = await ProductosModel.findOne({
+                        nombre: cliente.producto
+                    })
+
+                    if (!existeProducto) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'No existe el Producto que has ingresado, elige un Producto correcto',
+                        })
+                    }
+
+                    if(existeProducto.cantidad == 0){
+                        return reject({
+                            ok: false,
+                            mensaje: 'No se puede agregar el cliente porque se agotaron la cantidad de ese producto',
+                        })
+                    }
+
+                    const nuevoCliente = {
+                        nombre: cliente.nombre,
+                        apellido: cliente.apellido,
+                        cedula: cliente.cedula,
+                        producto: cliente.producto,
+                        gasto: existeProducto.precio
+                    }
+
+                    const clienteCreado = await ClientesModel.create(
+                        nuevoCliente
+                    )
+
+                    if (!clienteCreado) {
+                        return reject({
+                            ok: false,
+                            mensaje: 'Hubo un error al crear el Cliente',
+                        })
+                    }
+
+                    let nuevaCantidad = existeProducto.cantidad - 1
+                    await ProductosModel.updateOne({ nombre: cliente.producto }, { $set: {cantidad: nuevaCantidad} })
+
+                    resolve({
+                        ok: true,
+                        "Cliente": clienteCreado,
+                    })
+                }
+            } catch (error) {
+                console.error('Error al agregar el Cliente:', error)
+                return reject({
+                    ok: false,
+                    mensaje: 'Hubo un error al agregar el Cliente',
+                })
+            }
+        })
+    }
+
 }
 
 const clientesC = new Cliente();
--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,19 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const Clientes = require('../controller/clientes.controller')
+const checkAutenticacion = require('../controller/service/jwtAuth');
+
 
 // Listar Clientes
-router.get('/listar', function(req, res, next) {
-  
+router.get('/listar', function(req, res, next){
+    let roles = ["admin", "editor", "user"];
+    checkAutenticacion(req, res, next, roles);
+},
+
+function(req, res, next) {
+    Clientes.listar()
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 /* Crear Clientes. */
-router.post('/agregar', function(req, res, next) {
-  
-});
+router.post('/agregar', function(req, res, next){
+    let roles = ["admin", "editor"];
+    checkAutenticacion(req, res, next, roles);
+},
 
-// Eliminar Clientes
-router.delete('/eliminar', function(req, res, next) {
-  
+function(req, res, next) {
+    Clientes.agregar(req.body)
+    .then((resultado) => {
+        res.status(200).json({"status": 200, mensaje: resultado})
+    })
+    .catch((error) => {
+        res.status(400).json({"status": 400, mensaje: error})
+    })
 });
 
 module.exports = router;
